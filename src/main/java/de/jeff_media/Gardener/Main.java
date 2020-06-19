ee44756e2ce0a24096361db00ee3ccd6f384b0c3
JEFF <1122571+mfnalex@users.noreply.github.com>
2020-06-19 20:50:16 +0200

    config.yml

--- src/main/java/de/jeff_media/Gardener/Main.java
+++ src/main/java/de/jeff_media/Gardener/Main.java
@@ -3,4 +3,12 @@ package de.jeff_media.Gardener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+
+    public void onEnable() {
+        createConfig();
+    }
+
+    private void createConfig() {
+        saveDefaultConfig();
+    }
 }
